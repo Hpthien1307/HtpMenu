37e2c68f3c0a24c3fe4b8e0d2534c48061e57904
Huynh Phuc Thien <hpthien1307@gmail.com>
2026-06-25 10:24:01 +0700

    update port listen Backend

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
-  await app.listen(process.env.PORT ?? 3000)
+  const port = process.env.VERCEL ? (process.env.PORT || 3000) : 3000
+  await app.listen(port)
 }
 bootstrap()
